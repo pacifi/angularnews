277f41cdbbd1008fa5232d2576781cc25dc370ad
pacifi <pacifi.bnr@gmail.com>
2017-08-03 17:30:53 -0500

    Patron Revealing Module

--- app/categories/categoriesCtrl.js
+++ app/categories/categoriesCtrl.js
@@ -4,20 +4,36 @@
     "use strict";
 
     angular.module("newsWikiApp")
-        .controller("CategoriesCtrl", CategoriesCtrl)
+        .controller("CategoriesCtrl", CategoriesCtrl);
 
     function CategoriesCtrl(categoriesService) {
         var me = this;
 
-        categoriesService.then(function (response) {
-            me.categories = response.data;
-        });
-
         me.showCategories = false;
+        categoriesRepository.getCategories(
+            {
+                categoriesService: categoriesService,
+                success: function (data) {
+                    me.showCategories = data;
+                }
+            });
 
         me.toggleCategories = function () {
             me.showCategories = !me.showCategories;
         };
 
     }
+
+    //Patron Revealing Module
+    var categoriesRepository = (function () {
+        var me = {};
+        me.getCategories = function (options) {
+            options.categoriesService.then(function (response) {
+                options.success(response.data);
+            });
+        };
+        return {
+            getCategories: me.getCategories
+        };
+    }());
 }());
